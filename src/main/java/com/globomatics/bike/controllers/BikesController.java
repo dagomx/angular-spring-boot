23a63a728737d81f53cf7762845ca20f134dde60
Bravo Lopez, Lester D <lester.d.bravo.lopez@accenture.com>
2018-06-11 17:50:18 -0500

    Added put and delete methods to BikesController

--- src/main/java/com/globomatics/bike/controllers/BikesController.java
+++ src/main/java/com/globomatics/bike/controllers/BikesController.java
@@ -1,12 +1,18 @@
 package com.globomatics.bike.controllers;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,8 +28,13 @@ public class BikesController {
 	private BikeRepository bikeRepository;
 	
 	@GetMapping
-	public List<Bike> list(){
+	public List<Bike> getAll(){
 		return bikeRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Bike get(@PathVariable("id") long id) {
+		return bikeRepository.getOne(id);
 	}
 	
 	@PostMapping
@@ -32,8 +43,26 @@ public class BikesController {
 		bikeRepository.save(bike);
 	}
 	
-	@GetMapping("/{id}")
-	public Bike get(@PathVariable("id") long id) {
-		return bikeRepository.getOne(id);
+	@PutMapping("/{id}")
+	public Bike update(@PathVariable(value = "id") Long id,
+            @Valid @RequestBody Bike bikeUpdate) {
+		Optional<Bike> bike = bikeRepository.findById(id);
+		Bike foundBike = bike.get();
+		foundBike.setName(bikeUpdate.getName());
+		foundBike.setModel(bikeUpdate.getModel());
+		
+		bikeRepository.save(foundBike);
+		return foundBike;
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		Optional<Bike> foundBike;
+		if((foundBike = bikeRepository.findById(id)).isPresent()) {
+			Bike bike = foundBike.get();
+			bikeRepository.delete(bike);
+		}
+	    return ResponseEntity.ok().build();
+	}
+	
 }
